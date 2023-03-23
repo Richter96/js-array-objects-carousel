aefa7c3190997a6fafc83d098648b04933793093
Richter96 <castiglione96.rc@gmail.com>
2023-03-23 16:31:39 +0100

    new cicle

--- asset/js/main.js
+++ asset/js/main.js
@@ -12,8 +12,55 @@ Milestone 2:
 Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
  */
 
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
+
 
 // prendiamo elemento della dom nel quale insrire un markup
 const slider = document.querySelector('.slider')
+const markupCard = `
+    <div class="card">
+        <img src="./asset/img/03.webp" class="img-fluid" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
+</div>
+`
+let i = 0
+// creo un loop per inserire le immagini 
+images.forEach((element, index, image)=>{
+    console.log(element(index).image)
+})
 
-const markupCard = 
+
+
+
+
+
+
+function printToDom(markup, elementDom) {
+    elementDom.insertAdjacentHTML('beforeend', markup)
+}
